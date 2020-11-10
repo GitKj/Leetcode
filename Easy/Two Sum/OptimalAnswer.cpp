7e12eda6bcc297365d334b328222ef407c033abc
Kajoyrie P <32532509+GitKj@users.noreply.github.com>
2020-11-09 19:42:23 -0500

    Update OptimalAnswer.cpp

--- Easy/Two Sum/OptimalAnswer.cpp
+++ Easy/Two Sum/OptimalAnswer.cpp
@@ -1,12 +1,25 @@
+/*
+    What is different from MyAnswer.cpp?
+    > This solution uses only 1 pass (though 2 passes are still O(n)).
+    > Makes for cleaner, shorter code.
+*/
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
   
     unordered_map<int, int> result;
+        
+    // loop through the vector    
     for (int i = 0; i < nums.size(); i++)
     {
         int newTarg = target - nums[i];
-        if (result.count(
+        if (result.count(newTarg))
+        {
+            return { result.at(newTarg), i };
+        }
+        result.insert(make_pair(nums[i], i);
     }
-    
+    return {0, 0};
  }
