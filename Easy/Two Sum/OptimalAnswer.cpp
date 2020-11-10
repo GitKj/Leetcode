7540da4ff893a70e98737b24ab7f79c20e6c122b
Kajoyrie P <32532509+GitKj@users.noreply.github.com>
2020-11-09 19:38:16 -0500

    Create OptimalAnswer.cpp

---
+++ Easy/Two Sum/OptimalAnswer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+  
+    unordered_map<int, int> result;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int newTarg = target - nums[i];
+        if (result.count(
+    }
+    
+ }
